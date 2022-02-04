6d305f45778524cc595635e8aa88ff49cad5b13d
soobin <a01056239778@gmail.com>
2022-02-04 16:32:05 +0900

    Chapter06_확인문제 17 코드

---
+++ homework/ex0128_문수빈/chapter6_확인문제코드/Printer.java
@@ -0,0 +1,21 @@
+package ex0128_문수빈;
+
+public class Printer {
+    public static void print(String s) {
+        System.out.println(s);
+    }
+
+    public static void print(int i) {
+        System.out.println(i);
+    }
+
+    public static void print(double d) {
+        System.out.println(d);
+    }
+
+    public static void print(boolean b) {
+        System.out.println(b);
+    }
+}
+
+
---
+++ homework/ex0128_문수빈/chapter6_확인문제코드/PrinterExample.java
@@ -0,0 +1,8 @@
+package ex0128_문수빈;
+
+public class PrinterExample {
+    public static void main(String[] args) {
+        Printer.print(10);
+        Printer.print("홍길동");
+    }
+}
