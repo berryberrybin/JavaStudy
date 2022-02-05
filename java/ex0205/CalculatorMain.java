e5dcc5b1e901ba68dac85b60032982fbafb2700d
soobin <a01056239778@gmail.com>
2022-02-05 22:14:28 +0900

    #25 [Refactoring] 불필요한 변수 제거 / 메소드 추출 / 메소드명 명확히 변경

--- java/ex0205/CalculatorMain.java
+++ java/ex0205/CalculatorMain.java
@@ -8,59 +8,52 @@ public class CalculatorMain {
     public int calculate(int lastNumber, int threadNumber) {
         List<SumThread> sumThreadList = makeThreadList(lastNumber, threadNumber);
         executeThreadList(sumThreadList);
-        checkThreadEnd(sumThreadList, threadNumber);
-        return sumThreadResult(sumThreadList);
+        waitThreadList(sumThreadList);
+        return sumThreadListResult(sumThreadList);
     }
 
     private List<SumThread> makeThreadList(int lastNumber, int threadNumber) {
         List<SumThread> threadList = new ArrayList<>();
-        int start = 1;
         int range = (lastNumber / threadNumber);
-        int end = range;
         for (int i = 0; i < threadNumber; i++) {
+            SumThread thread;
             if (i == threadNumber - 1) {
-                end = lastNumber;
+                thread = new SumThread((range * i) + 1, lastNumber);
+            } else {
+                thread = new SumThread((range * i) + 1, (range * (i + 1)));
             }
-            SumThread thread = new SumThread(start, end);
             threadList.add(thread);
-            start = end + 1;
-            end += range;
         }
         return threadList;
     }
 
     private void executeThreadList(List<SumThread> sumThreadList) {
-        for (int i = 0; i < sumThreadList.size(); i++) {
-            sumThreadList.get(i).start();
+        for (SumThread sumThread : sumThreadList) {
+            sumThread.start();
         }
     }
 
-    private void checkThreadEnd(List<SumThread> threadList, int threadNumber) {
-        int count = 0;
-        boolean run = true;
-        while (run) {
-            for (int i = 0; i < threadList.size(); i++) {
-                Thread.State state = threadList.get(i).getState();
-                if (!state.equals(Thread.State.TERMINATED)) {
-                    break;
-                } else {
-                    count++;
-                }
-            }
-            if (count == threadNumber) {
-                run = false;
-            }
-        }
+    private void waitThreadList(List<SumThread> threadList) {
+        while (!isDoneThreadList(threadList)) ;
     }
 
-    private int sumThreadResult(List<SumThread> threadList) {
+    private boolean isDoneThreadList(List<SumThread> threadList) {
+        for (SumThread sumThread : threadList) {
+            Thread.State state = sumThread.getState();
+            if (!state.equals(Thread.State.TERMINATED)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int sumThreadListResult(List<SumThread> threadList) {
         int result = 0;
-        for (int i = 0; i < threadList.size(); i++) {
-            result += threadList.get(i).getResult();
+        for (SumThread sumThread : threadList) {
+            result += sumThread.getResult();
         }
         return result;
     }
-
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
