dae9f701c12538fb17ca33b8ec8a0e236e7a4065
soobin <a01056239778@gmail.com>
2022-02-09 00:41:57 +0900

    #31 instanceof 예제

---
+++ homework/ex0208_문수빈/castingExample/InstanceofExample.java
@@ -0,0 +1,30 @@
+package ex0208_문수빈.castingExample;
+
+public class InstanceofExample {
+    public static void method1(Parent parent) {
+        if (parent instanceof Child) {
+            Child child = (Child) parent;
+            System.out.println("method1 - child로 변환 성공");
+        } else {
+            System.out.println("method1 - child로 변환되지 않음");
+        }
+    }
+
+    public static void method2(Parent parent) {
+        Child child = (Child) parent;
+        System.out.println("method2-child로 변환 성공");
+    }
+
+    public static void main(String[] args) {
+        Parent parentA = new Child();
+
+        method1(parentA); // Child로 변환 성공
+        method2(parentA); // Child로 변환 성공
+
+
+        Parent parentB = new Parent();
+        method1(parentB); // Child로 변환되지 않음
+        method2(parentB); // classCastException 발생함
+
+    }
+}
