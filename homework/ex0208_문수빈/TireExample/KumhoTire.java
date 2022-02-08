f1517878f6a643b1a1928847156e035087d2d11c
soobin <a01056239778@gmail.com>
2022-02-09 00:02:36 +0900

    #31 교재 상속 예제 코드

---
+++ homework/ex0208_문수빈/TireExample/KumhoTire.java
@@ -0,0 +1,20 @@
+package ex0208_문수빈.TireExample;
+
+public class KumhoTire extends Tire {
+    public KumhoTire(String location, int maxRotation) {
+        super(location, maxRotation);
+    }
+
+    @Override
+    public boolean roll() {
+        ++accumulateRotation;
+        if (accumulateRotation < maxRotation) {
+            System.out.println(location + "kumhoTire 수명 : " + (maxRotation - accumulateRotation) + "회");
+            return true;
+        } else {
+            System.out.println("**" + location + "kumhoTire 펑크 **");
+            return false;
+        }
+    }
+
+}
